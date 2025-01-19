1d757950b4ad7c4620c9b79371fb551c664437e5
Sumit-Shastri <shastrisumit25@gmail.com>
2025-01-19 19:36:10 +0530

    Created a function to find null values if any, No values found its a good start !

--- Zomato Data Analysis/main.py
+++ Zomato Data Analysis/main.py
@@ -1,7 +1,12 @@
 import pandas
 import duckdb
 
-data = pandas.read_csv(r"E:\Personal Projects\RAW DATA\Zomato-data-.csv").df()
+data = pandas.read_csv(r"E:\Personal Projects\RAW DATA\Zomato-data-.csv")
 print(data)
 
-
+def null_finder(x):
+                if x.isnull().values.any():
+                    print("The file contains null data")
+                else:
+                    print("No null data found")
+null_finder(data)
